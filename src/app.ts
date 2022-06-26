b4445bae97d64b7f84c844b94fcd32ec31d25b67
dgeorgiev436 <dgeorgiev436@gmail.com>
2022-06-26 01:58:08 +0000

    Render form with ts class

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-// Code goes here!
+
+class ProjectUnput {
+	
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLFormElement;
+	
+	constructor(){
+// 		Get elements by ID
+		this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
+		this.hostElement = document.getElementById("app")! as HTMLDivElement;
+		
+		const importedNode = document.importNode(this.templateElement.content, true);
+		this.element = importedNode.firstElementChild as HTMLFormElement;
+		
+		this.attach();
+	}
+	
+// 	Attach template to host
+	private attach(){
+		this.hostElement.insertAdjacentElement("afterbegin", this.element);
+	}
+}
+
+
+const prjInput = new ProjectUnput();
