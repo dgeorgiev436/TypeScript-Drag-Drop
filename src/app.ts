319a47587e3a1850ff41cf9742afcc9ffa6d3d31
dgeorgiev436 <dgeorgiev436@gmail.com>
2022-06-26 02:48:47 +0000

    Interacting with DOM

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 
-class ProjectUnput {
+class ProjectInput {
 	
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
 	
 	constructor(){
 // 		Get elements by ID
@@ -12,15 +15,40 @@ class ProjectUnput {
 		
 		const importedNode = document.importNode(this.templateElement.content, true);
 		this.element = importedNode.firstElementChild as HTMLFormElement;
+		this.element.id = "user-input";
 		
+		this.titleInputElement = document.querySelector('#title')! as HTMLInputElement;
+		this.descriptionInputElement = document.querySelector("#description")! as HTMLInputElement;
+		this.peopleInputElement = document.querySelector("#people")! as HTMLInputElement;
+		
+		this.configure();
 		this.attach();
 	}
 	
+		// Submit handler logic
+	// private submitHandler(event: Event){
+	// 	event.preventDefault();
+		
+	// 	console.log(this.titleInputElement.value);
+	// }
+	
+	private submitHandler = (event : Event) => {
+		event.preventDefault();
+		
+		console.log(this.titleInputElement.value);
+	}
+
+
+// 	Using bind(this) to make the this keyword in the submitHandler refer to the Class
+	private configure = () => {
+		this.element.addEventListener("submit", this.submitHandler);
+	}
+	
 // 	Attach template to host
-	private attach(){
+	private attach() = () => { 
 		this.hostElement.insertAdjacentElement("afterbegin", this.element);
 	}
 }
 
 
-const prjInput = new ProjectUnput();
+const prjInput = new ProjectInput();
